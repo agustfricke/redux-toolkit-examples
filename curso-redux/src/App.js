c578e89c4c2f515ce8b1c8edb5a18570a59d236a
Agust Fricke <agustfricke@protonmail.com>
2023-02-20 23:03:30 -0300

    inicial 04

--- curso-redux/src/App.js
+++ curso-redux/src/App.js
@@ -1,12 +1,7 @@
-import PostsList from "./components/PostsList";
-import AddPost from "./components/AddPost";
-
 function App() {
   
   return (
     <div className="bg-gray-900 min-h-screen">
-      <AddPost/>
-      <PostsList/>
     </div>
   );
 }
